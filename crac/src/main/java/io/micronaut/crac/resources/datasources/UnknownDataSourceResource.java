15e7eedb92eacc8e1dc0d4d3f0016a197c98c4eb
Tim Yates <tim.yates@gmail.com>
2022-09-05 16:21:51 +0100

    Add handler for HikariDataSource
    We have to generate a handler for each DataSource bean, but this currently only handles Hikari
    Other DataSources will be done in separate PRs
    
    On shutdown, we first suspend the pool, then request the closure of all connections and wait 30s for them all
    to close.
    
    When we restore, we simply resume the pool.
    
    We cannot close the pool as this invalidates it, and it cannot be re-opened.
    
    This also requires the configuration:
    
    ```
    datasources.default.allow-pool-suspension: true
    ```
    
    To allow pool suspension

---
+++ crac/src/main/java/io/micronaut/crac/resources/datasources/UnknownDataSourceResource.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.crac.resources.datasources;
+
+import org.crac.Context;
+import org.crac.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+
+/**
+ * Handler for currently unhandled DataSources.
+ *
+ * @author Tim Yates
+ * @since 1.1.0
+ */
+public class UnknownDataSourceResource implements Resource {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UnknownDataSourceResource.class);
+
+    private final DataSource dataSource;
+
+    public UnknownDataSourceResource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        if (LOG.isErrorEnabled()) {
+            LOG.error("Cannot suspend DataSource {}", dataSource);
+        }
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
+        if (LOG.isErrorEnabled()) {
+            LOG.error("Cannot resume DataSource {}", dataSource);
+        }
+    }
+}
