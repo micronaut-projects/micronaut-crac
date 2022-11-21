e40ca879f239b8db6e88b2e5b23adc5ba65ac636
Tim Yates <tim.yates@gmail.com>
2022-11-21 13:27:55 +0000

    Switch to using the latest build structure
    The old settings plugin was pulling in an old snapshot, so we got an incompatible logback.
    
    This change switched to use the latest settings plugin, and imports the catalogs for the
    required sql and serde libs.

--- crac/build.gradle.kts
+++ crac/build.gradle.kts
@@ -1,16 +1,3 @@
 plugins {
     id("io.micronaut.build.internal.crac-module")
 }
-
-dependencies {
-    testImplementation(mn.micronaut.serde.jackson)
-}
-
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.group == "ch.qos.logback") {
-            useVersion("1.4.5")
-            because("Currently micronaut-bom is pulling in a version that requires 1.x slf4j, not 2.x")
-        }
-    }
-}
