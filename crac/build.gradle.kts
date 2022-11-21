9ed638c283f349471d5938b9cf08ce519b84d4a9
Tim Yates <tim.yates@gmail.com>
2022-11-21 12:19:13 +0000

    Use the platform BOM to get the correct version of logback-classic

--- crac/build.gradle.kts
+++ crac/build.gradle.kts
@@ -5,12 +5,3 @@ plugins {
 dependencies {
     testImplementation(mn.micronaut.serde.jackson)
 }
-
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.group == "ch.qos.logback") {
-            useVersion("1.4.5")
-            because("Currently micronaut-bom is pulling in a version that requires 1.x slf4j, not 2.x")
-        }
-    }
-}
