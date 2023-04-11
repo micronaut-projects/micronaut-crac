e2ff115a8c729966f6ab8d4b1cd91d5b455791e1
Tim Yates <tim.yates@gmail.com>
2023-04-11 09:13:18 +0100

    Also destroy StatefulRedisPubSubConnectionResource beans
    As raised here https://github.com/micronaut-projects/micronaut-crac/issues/63#issuecomment-1494710527
    
    StatefulRedisPubSubConnection beans were not destroyed during checkpointing, and so we were getting connection closed errors.

---
+++ test-suite-redis/src/test/groovy/io/micronaut/crac/RedisStatefulPubSubConnectionSpec.groovy
@@ -0,0 +1,45 @@
+package io.micronaut.crac
+
+import ch.qos.logback.classic.Logger
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.pubsub.StatefulRedisPubSubConnection
+import io.micronaut.context.annotation.Property
+import io.micronaut.core.util.StringUtils
+import jakarta.inject.Inject
+import org.slf4j.LoggerFactory
+
+@Property(name = "spec.name", value = "RedisCracSpec")
+@Property(name = "redis.cache.enabled", value = StringUtils.TRUE)
+class RedisStatefulPubSubConnectionSpec extends BaseCacheSpecification {
+
+    @Inject
+    StatefulRedisPubSubConnection<String, String> connection;
+
+    void "test redis connection"() {
+        given:
+        Logger l = (Logger) LoggerFactory.getLogger("io.micronaut.crac.resources")
+        l.addAppender(appender)
+        appender.start()
+
+        when:
+        connection.sync().set("foo", "bar")
+
+        then:
+        connection.sync().get("foo") == "bar"
+
+        when:
+        simulator.runBeforeCheckpoint()
+
+        then:
+        appender.events.formattedMessage.any { it.contains("Destroying Redis stateful connection") }
+
+        when: "we trigger a restore"
+        simulator.runAfterRestore()
+
+        and: "we get a new connection"
+        def newConnection = ctx.getBean(StatefulRedisPubSubConnection.class)
+
+        then: "the data is still there"
+        newConnection.sync().get("foo") == "bar"
+    }
+}
