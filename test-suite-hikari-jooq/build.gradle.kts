c1910ad50565ca6e9219143fa925d320d94ad022
Tim Yates <tim.yates@gmail.com>
2023-04-25 12:11:04 +0100

    Update versions and fix build

--- test-suite-hikari-jooq/build.gradle.kts
+++ test-suite-hikari-jooq/build.gradle.kts
@@ -1,26 +1,11 @@
 plugins {
     id("groovy")
-    id("io.micronaut.library") version "4.0.0-SNAPSHOT"
-}
-
-// Required as there's no M1 platform yet
-micronaut {
-    this.version("4.0.0-SNAPSHOT")
+    id("io.micronaut.build.internal.crac-library-test-suite")
 }
 
 dependencies {
-    testImplementation(projects.micronautCrac)
-    testImplementation(mnTest.micronaut.test.spock)
-    testImplementation(mn.micronaut.http.server.netty)
-    testImplementation(mn.micronaut.inject.groovy)
     testImplementation(mnSql.micronaut.jooq)
     testImplementation(mnSql.micronaut.jdbc.hikari)
-    testImplementation(mn.logback.classic)
+
     testRuntimeOnly(mnSql.h2)
-    testRuntimeOnly(mn.micronaut.jackson.databind)
 }
-
-tasks.test {
-    useJUnitPlatform()
-}
-
