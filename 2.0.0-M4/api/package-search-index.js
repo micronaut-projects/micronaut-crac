5290123b0c2ad5c0bd7dbb8fdf4c240df199cc85
sdelamo <sdelamo@users.noreply.github.com>
2023-06-04 11:42:07 +0000

    Deploying to gh-pages - 11:42:07

---
+++ 2.0.0-M4/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.crac"},{"l":"io.micronaut.crac.events"},{"l":"io.micronaut.crac.resources"},{"l":"io.micronaut.crac.resources.datasources"},{"l":"io.micronaut.crac.resources.datasources.resolver"},{"l":"io.micronaut.crac.resources.redis"},{"l":"io.micronaut.crac.test"}];updateSearchResults();
