376e868ac632c691fb0885889e6de914d181fcab
Tim Yates <tim.yates@gmail.com>
2023-10-06 11:09:13 +0100

    Add test-suite-kotlin-ksp and exclude test-suite-kotlin for Java 21+

---
+++ test-suite-kotlin-ksp/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("com.google.devtools.ksp")
+    id("io.micronaut.build.internal.crac-test-suite")
+}
+
+dependencies {
+    ksp(mn.micronaut.inject.kotlin)
+
+    testImplementation(projects.micronautCrac)
+    testImplementation(mnTest.micronaut.test.junit5)
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
