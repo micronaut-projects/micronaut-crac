3035e2ca08cb2fbdcb33c952fc5fed2287937021
Tim Yates <tim.yates@gmail.com>
2023-10-18 10:03:26 +0100

    Up to 6.6.0 shared plugin to run tests on 21

--- test-suite-kotlin/build.gradle.kts
+++ test-suite-kotlin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("org.jetbrains.kotlin.kapt")
@@ -9,4 +11,13 @@ dependencies {
     kaptTest(mn.micronaut.inject.java)
     testImplementation(mnTest.micronaut.test.junit5)
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
